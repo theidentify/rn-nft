ada3082e755104813f1254aa5fc5315e8f918a8f
Krisanathep Boonpromma <krisanathep.b@gli.co.th>
2022-10-13 09:37:35 +0700

    Complete course project

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useIsFocused } from '@react-navigation/core';
+
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused();
+
+  if (!isFocused) return null;
+
+  return <StatusBar animated={true} {...props} />;
+};
+
+export default FocusedStatusBar;
---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './NFTCard';
+import { SubInfo } from './SubInfo';
+import { CircleButton, RectButon } from './Button';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButon,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid,
+};
